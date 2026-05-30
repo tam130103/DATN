ca1482c2f87a2a09038721f588544086204a6ff2
tam130103 <thetam2103@gmail.com>
2026-05-30 15:35:02 +0700

    feat(frontend): setup vercel web analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
         <Toaster
           position="top-right"
           toastOptions={{
